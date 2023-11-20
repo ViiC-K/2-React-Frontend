548b41293ad7aea3359153b8b177b0330a3a42ee
viic007 <viickub@gmail.com>
2023-11-20 14:44:09 +0700

    change footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
 import Navbar from "./Component/Navbar/Navbar";
-import Footer from "./Component/footer/Footer";
 import Gallery from "./Component/Gallery/Gallery";
+
+
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <div className=" px-5 pt-4">
         <Navbar />
         <Gallery />
-        <Footer />
+      
       </div>
     </>
   );
